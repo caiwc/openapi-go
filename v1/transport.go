e13cbfc876c5f73484c109d34434983b10cab3e2
wccai <wccai@easyops>
2025-11-22 22:10:49 +0800

    chore(): add log

--- v1/transport.go
+++ v1/transport.go
@@ -10,14 +10,14 @@ type TransportOption func(*transport)
 
 type transport struct {
 	rt  http.RoundTripper
-	sig ApiKey
+	sig Signer
 }
 
 func (t *transport) RoundTrip(req *http.Request) (*http.Response, error) {
 	if err := t.sig.Sign(time.Now(), request{req}); err != nil {
 		return nil, err
 	}
-	log.Printf("full url: %s , accessKey: %s, secretKey: %s \n", req.URL.String(), t.sig.AccessKey, t.sig.SecretKey)
+	log.Printf("full url: %s\n", req.URL.String())
 	return t.rt.RoundTrip(req)
 }
 
